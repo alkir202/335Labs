c7f8a3f073ff81b3fdbedd3d5cf19875fe87e0c4
dimaagafetov <dimaagafetov2002@mail.ru>
2020-10-19 01:25:09 +0300

    finish

---
+++ 335Labs/Agafetov/Metod.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace _335Labs.Agafetov
+{
+    class Metod
+    {
+                public static double PloshadOkruzhnosti(double a)
+                {
+                    double s = 3.14 * a * a;
+                    Console.WriteLine(s);
+                    return 0;
+                }
+                public static double dlinaokruzhnosti(double a)
+                {
+
+                    double l = 2 * 3.14 * a;
+                    Console.WriteLine(l);
+                    return 0;
+                }
+
+                public static double teoremaPif(double a, double b)
+                {
+                    double c = a * a + b * b;
+                    double d = Math.Sqrt(c);
+                    Console.WriteLine(d);
+                    return 0;
+                }
+
+                public static (double, double) discriminant(double a, double b, double c)
+                {
+                    double D = b * b - 4 * a * c;
+                    double result = 0;
+                    if (D >= 0)
+
+                    {
+
+                        double x1 = (-b - Math.Sqrt(D)) / (2 * a);
+                        double x2 = (-b + Math.Sqrt(D)) / (2 * a);
+                        return (x1, x2);
+                    }
+
+                    else
+                    {
+                        Console.WriteLine($"no Sqrt");
+                    }
+                    return (0, 0);
+                }
+    
+                public static double silatoka(double a, double b)
+                {
+                    double i = a / b;
+                    return i;
+
+
+                }
+                public static double soprotivlenie(double a, double b)
+                {
+                    double r = a / b;
+                    return r;
+                }
+                public static double napryazhenie(double a, double b)
+                {
+                    double v = a * b;
+                    return v;
+                }
+            }
+}
+
+
--- 335Labs/Program.cs
+++ 335Labs/Program.cs
@@ -1,4 +1,4 @@
-﻿using _335Labs.Nacharov;
+﻿using _335Labs.Agafetov;
 using System;
 
 namespace _335Labs
@@ -6,7 +6,15 @@ namespace _335Labs
     class Program
     {
         static void Main(string[] args)
-        { 
+
+        {
+            Console.WriteLine(Metod.PloshadOkruzhnosti (10));
+            Console.WriteLine(Metod.dlinaokruzhnosti(7));
+            Console.WriteLine(Metod.teoremaPif(6, 6));
+            Console.WriteLine(Metod.discriminant(1, 8, 5));
+            Console.WriteLine(Metod.silatoka(5, 5));
+            Console.WriteLine(Metod.soprotivlenie(1, 1));
+            Console.WriteLine(Metod.napryazhenie(7, 4));
         }
     }
 }
