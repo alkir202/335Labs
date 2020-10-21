fa1e1613fe1b3e028813630ecd0a549bfb80bb96
Dinaremba <bot3.steam@mail.ru>
2020-10-21 23:09:40 +0300

    finish(owibki)

---
+++ 335Labs/Nizamov/Sstring.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _335Labs.Nizamov
+{
+    class Sstring
+    {
+        public static string Ranzniestroki(string a)
+        {
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+
+                    Console.Write(a[i] + " ");
+                }
+                else if (i % 2 != 0)
+                    Console.WriteLine(a[i]);
+
+            }
+            return " ";
+        }
+
+        public static string odderandevenodinaksimvoli(string a)
+        {
+            string odd = null;
+            string even = null;
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (i % 2 == 0)
+
+                {
+                    even += a[i];
+                    Console.WriteLine(even);
+                }
+                else
+                    odd += a[i];
+                Console.WriteLine(odd);
+            }
+            return " ";
+        }
+
+        public static string palindrom(string a)
+        {
+            string res = null;
+            a = a.Replace(" ", " ").ToLower();
+            for (int i = 0; i < a.Length; i++)
+            {
+                res += a[a.Length - 1 - i];
+            }
+            if (a == res)
+            {
+                Console.WriteLine("+");
+            }
+            else
+                Console.WriteLine("-");
+            return " ";
+        }
+
+    }
+}
+
+
+
--- 335Labs/Program.cs
+++ 335Labs/Program.cs
@@ -7,24 +7,27 @@ namespace _335Labs
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(MathMetod.Okryjnost(228));
-            Console.WriteLine();
-            Console.WriteLine(MathMetod.dlinaokryj(5));
-            Console.WriteLine();
-            Console.WriteLine(MathMetod.pif(2, 5));
-            Console.WriteLine();
-            Console.WriteLine(MathMetod.diskrim(1,7,3));
-            Console.WriteLine();
-            Console.WriteLine(MathMetod.divisioninterval(-2,2,5));
-            Console.WriteLine();
-            Console.WriteLine(MathMetod.Obiemparrall(2,5));
-            Console.WriteLine();
-            Console.WriteLine(MathMetod.tok(2, 5));
-            Console.WriteLine();
-            Console.WriteLine(MathMetod.napryajenie(2, 5));
-            Console.WriteLine();
-            Console.WriteLine(MathMetod.soprotiv(2, 5));
-            Console.WriteLine();
+            //    Console.WriteLine(MathMetod.Okryjnost(228));
+            //    Console.WriteLine();
+            //    Console.WriteLine(MathMetod.dlinaokryj(5));
+            //    Console.WriteLine();
+            //    Console.WriteLine(MathMetod.pif(2, 5));
+            //    Console.WriteLine();
+            //    Console.WriteLine(MathMetod.diskrim(1,7,3));
+            //    Console.WriteLine();
+            //    Console.WriteLine(MathMetod.divisioninterval(-2,2,5));
+            //    Console.WriteLine();
+            //    Console.WriteLine(MathMetod.Obiemparrall(2,5));
+            //    Console.WriteLine();
+            //    Console.WriteLine(MathMetod.tok(2, 5));
+            //    Console.WriteLine();
+            //    Console.WriteLine(MathMetod.napryajenie(2, 5));
+            //    Console.WriteLine();
+            //    Console.WriteLine(MathMetod.soprotiv(2, 5));
+            //    Console.WriteLine();
+            string a = Goroddorog;
+            Console.WriteLine(Sstring.odderandevenranzniestroki(a));
         }
+
     }
 }
